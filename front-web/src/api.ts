2db1c2d6ee74a11c0e7c00b31cbed4911ba7a032
JoaoOtavioSegantini <joaotavioos@outlook.com>
2021-01-08 22:56:18 -0300

    Finish front-web

--- front-web/src/api.ts
+++ front-web/src/api.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
+import { OrderPayload } from "./Orders/types";
 
-const API_URL = process.env.REACT_APP_API_URL;
+const API_URL = process.env.REACT_APP_url;
 const mapboxToken = process.env.REACT_APP_ACCESS_TOKEN_MAP_BOX;
 
 export async function  fetchProducts() {
@@ -9,4 +10,8 @@ export async function  fetchProducts() {
 
 export function fetchLocalMapBox(local: string) {
   return axios(`https://api.mapbox.com/geocoding/v5/mapbox.places/${local}.json?access_token=${mapboxToken}`)
+}
+
+export function saveOrder(payload: OrderPayload) {
+  return axios.post(`${API_URL}/orders`, payload);
 }
